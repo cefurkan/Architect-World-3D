daa751b7652b54699ccc8b10e22134f858bf701a
Furkan Ceylan <furkancylan@gmail.com>
2021-03-08 19:02:11 +0300

    PlayerController Added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    float horizontal;
+    float vertical;
+    private Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 origin = Vector3.zero;
+
+        Quaternion hq = Quaternion.AngleAxis(-horizontal, Vector3.up);
+        Quaternion vq = Quaternion.AngleAxis(vertical, Vector3.right);
+
+        Quaternion q = hq * vq;
+
+        rb.MovePosition(q * (rb.transform.position - origin) + origin);
+        transform.rotation = Quaternion.LookRotation(transform.position - (transform.forward * (horizontal + vertical)).normalized);
+       // transform.LookAt(Vector3.zero);
+
+    }
+}
