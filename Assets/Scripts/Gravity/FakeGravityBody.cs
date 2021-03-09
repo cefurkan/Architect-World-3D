498b1479407f5729a1d1e3b66209d591eb93b958
Furkan Ceylan <furkancylan@gmail.com>
2021-03-09 16:30:23 +0300

    Gravity & Folder Editings

---
+++ Assets/Scripts/Gravity/FakeGravityBody.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Bu scriptin olduğu gameobjelerde muhakkak RigidBody olmalı
+[RequireComponent(typeof(Rigidbody))]
+public class FakeGravityBody : MonoBehaviour
+{
+    public FakeGravityAttractor attractor;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+        rb.useGravity = false;
+    }
+    private void Update()
+    {
+        attractor.Attract(transform);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Space]
+    [SerializeField]
+    private float moveSpeed = 1f;
+
+    [Space]
+    [SerializeField]
+    private float moveRotateSpeed = 50f;
+
+    float rotateSpeed = 50f;
+
+    Vector3 firstTouchPos = Vector3.zero;
+    Vector3 deltaTouchPos = Vector3.zero;
+    Vector3 direction = Vector3.zero;
+
+    [SerializeField]
+    float range = 50f;
+    void Start()
+    {
+     
+    }
+
+ 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            firstTouchPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            deltaTouchPos = Input.mousePosition - firstTouchPos;
+            direction = new Vector3(deltaTouchPos.x, 0f, deltaTouchPos.y);
+            //  Debug.Log(direction+"direction");
+
+            Vector3 moveForward = new Vector3(0f, 0f, direction.z).normalized;
+            Vector3 moveRotate = new Vector3(0f, direction.x, 0f).normalized;
+
+
+            if (Mathf.Abs(deltaTouchPos.y) >= range * .5)
+            {
+                transform.Translate(moveForward * moveSpeed * Time.deltaTime);
+
+            }
+
+            if (Mathf.Abs(deltaTouchPos.x) >= range)
+            {
+                transform.Rotate(moveRotate * moveRotateSpeed * Time.deltaTime);
+            }
+        }
+        
+    }
+}
+  
+
