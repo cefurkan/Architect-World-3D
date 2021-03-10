343f77a840603ea4129f6de482893309c51b8ea2
Cansutunc <cansutunc00@gmail.com>
2021-03-10 18:46:21 +0300

    UI and Game Manager

---
+++ Assets/Scripts/GameManager/GameHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public enum GameState
+{
+    Default,
+    Tutorial,
+    InGame,   
+}
+public class GameHandler : MonoBehaviour
+{
+    public UnityAction<GameState, GameState> OnGameSateChange;
+    public GameState gameState;
+    private static GameHandler instance;
+    public static GameHandler Instance => instance;
+    private void Start()
+    {
+        instance = this;
+        gameState = GameState.Default;
+
+    }
+    public void ChangeGameState(GameState newState)
+    {
+        GameState oldGameState = gameState;
+        gameState = newState;
+        OnGameSateChange?.Invoke(oldGameState,gameState);
+
+    }
+
+}
+
---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    public Text woodText;
+   
+    private void UpdateWoodCounter()
+    {
+        woodText.text = PlayerController.Instance.wood.amount.ToString();
+    }
+    private void Update()
+    {
+        UpdateWoodCounter();
+    }
+}
