c1c392cc706d916e30eff161e80fcea1ff2fb9af
Furkan Ceylan <furkancylan@gmail.com>
2021-03-11 09:26:02 +0300

    Stackable Logs && UI Manager

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,25 +7,24 @@ public class PlayerController : MonoBehaviour
     private static PlayerController instance;
     public static PlayerController Instance => instance;
 
-    [Space]
     private float moveSpeed = 7.5f;
-
-    [Space]
-    [SerializeField]
-    private float moveRotateSpeed = 50f;
 
     Vector3 firstTouchPos = Vector3.zero;
     Vector3 deltaTouchPos = Vector3.zero;
     Vector3 direction = Vector3.zero;
 
-    public Animator anim;
-
-    public ResourcesSO wood;
-
-    public Transform modelRoot;
-
+    [Space]
     [SerializeField]
     float range = 50f;
+    [Space]
+    public Animator anim;
+    [Space]
+    public ResourcesSO wood;
+    [Space]
+    public Transform modelRoot;
+
+    public GameObject[] stackableLogs;
+
 
     void Start()
     {
@@ -36,12 +35,14 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+        StackLog();
     }
 
     private void Move()
     {
         float svermingSpeedz = direction.z * .003f;
         float svermingSpeedx = direction.x * .003f;
+        moveSpeed = 5f;
 
 
         if (Input.GetMouseButtonDown(0))
@@ -59,9 +60,8 @@ public class PlayerController : MonoBehaviour
             Vector3 moveRight = new Vector3(direction.x, 0f, 0f).normalized;
 
             direction.z = Mathf.Abs(direction.z);
-            if (direction.z >= range * .5)
+            if (direction.z >= range)
             {
-                moveSpeed = svermingSpeedz * 2.5f;
                 modelRoot.transform.localRotation = Quaternion.Euler(0, 0f, 0);
                 transform.Translate(moveForward * moveSpeed * Time.deltaTime);
                 anim.SetFloat("MoveSpeed", svermingSpeedz);
@@ -78,7 +78,7 @@ public class PlayerController : MonoBehaviour
                 transform.Translate(moveRight * moveSpeed * Time.deltaTime);
                 if (deltaTouchPos.x > range)
                 {
-                    moveSpeed = svermingSpeedx * 3;
+                    // moveSpeed = svermingSpeedx * 3;
 
                     modelRoot.transform.localRotation = Quaternion.Euler(0, 90f, 0);
                     anim.SetFloat("MoveSpeed", svermingSpeedx);
@@ -86,7 +86,8 @@ public class PlayerController : MonoBehaviour
                 }
                 else if (deltaTouchPos.x < range)
                 {
-                    moveSpeed = -svermingSpeedx * 3;
+                    // moveSpeed = -svermingSpeedx * 3;
+
                     anim.SetFloat("MoveSpeed", -svermingSpeedx);
                     modelRoot.transform.localRotation = Quaternion.Euler(0, -90f, 0);
                 }
@@ -99,6 +100,38 @@ public class PlayerController : MonoBehaviour
         }
 
 
+    }
+
+    private void StackLog()
+    {
+        if (wood.amount < 10)
+        {
+            foreach (GameObject stackableLog in stackableLogs)
+            {
+                Debug.Log("kapalı all");
+                stackableLog.gameObject.SetActive(false);
+            }
+        }
+        if (wood.amount >= 10)
+        {
+            stackableLogs[0].gameObject.SetActive(true);
+        }
+        if (wood.amount >= 20)
+        {
+            stackableLogs[1].gameObject.SetActive(true);
+        }
+        if (wood.amount >= 30)
+        {
+            stackableLogs[2].gameObject.SetActive(true);
+        }
+        if (wood.amount >= 40)
+        {
+            stackableLogs[3].gameObject.SetActive(true);
+        }
+        if (wood.amount >= 50)
+        {
+            stackableLogs[4].gameObject.SetActive(true);
+        }
     }
 }
 
